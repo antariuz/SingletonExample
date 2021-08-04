1dd913635ba322fe3bb46b3fac244c1911f9666a
Tymur Foshch <antariuz@gmail.com>
2021-08-04 16:34:03 +0300

    refactored

--- src/main/java/SingletonExample.java
+++ src/main/java/SingletonExample.java
@@ -2,9 +2,9 @@ import singleton.Scooter;
 
 public class SingletonExample {
     public static void main(String[] args) {
-        Scooter scooter = Scooter.getInstance();
-        Scooter scooter2 = Scooter.getInstance();
-        System.out.println(scooter);
-        System.out.println(scooter2);
+        Scooter scooter = Scooter.getInstance("TERMINATOR T-800");
+        Scooter scooter2 = Scooter.getInstance("TERMINATOR T-1000");
+        System.out.println(scooter.getBrandName() + "\n" +
+                scooter2.getBrandName());
     }
 }
--- src/main/java/singleton/Scooter.java
+++ src/main/java/singleton/Scooter.java
@@ -1,14 +1,20 @@
 package singleton;
 
 public class Scooter {
-    private static Scooter instance = null;
+    private static Scooter instance;
+    private String brandName;
 
-    public Scooter() {
+    public Scooter(String brandName) {
+        this.brandName = brandName;
     }
 
-    public static Scooter getInstance() {
-        if (instance == null) instance = new Scooter();
+    public static Scooter getInstance(String brandName) {
+        if (instance == null) instance = new Scooter(brandName);
         return instance;
+    }
+
+    public String getBrandName() {
+        return brandName;
     }
 
 }
