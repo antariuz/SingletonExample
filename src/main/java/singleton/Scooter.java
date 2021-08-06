a6ccfd18663870828c152a2cd9092c516b8dcc89
Tymur Foshch <antariuz@gmail.com>
2021-08-07 00:16:47 +0300

    refactored

--- src/main/java/singleton/Scooter.java
+++ src/main/java/singleton/Scooter.java
@@ -4,13 +4,13 @@ public class Scooter {
     private static Scooter instance;
     private final String brandName;
 
-    public Scooter(String brandName) {
-        this.brandName = brandName;
-    }
-
     public static Scooter getInstance(String brandName) {
         if (instance == null) instance = new Scooter(brandName);
         return instance;
+    }
+
+    public Scooter(String brandName) {
+        this.brandName = brandName;
     }
 
     public String getBrandName() {
