9b76103e8c73c8c90cd783bb76c19b3fdee17683
Tymur Foshch <antariuz@gmail.com>
2021-08-07 00:30:00 +0300

    refactored

--- src/main/java/SingletonExample.java
+++ src/main/java/SingletonExample.java
@@ -4,7 +4,9 @@ public class SingletonExample {
     public static void main(String[] args) {
         Scooter scooter = Scooter.getInstance("TERMINATOR T-800");
         Scooter scooter2 = Scooter.getInstance("TERMINATOR T-1000");
+
         System.out.println(scooter.getBrandName() + "\n" +
                 scooter2.getBrandName());
+
     }
 }
